888495036f44f819b2464e0c45bca49aa482fd10
maxiaohao <maxiaohao@gmail.com>
2016-10-25 15:38:21 +0800

    Add redis conf

---
+++ easybuy-distributed/src/main/script/cluster-conf/redis-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this file should only be called in 'redis-dockerfile', to setup fastdfs and its nginx module in docker image
+
+cp /etc/redis.conf /etc/redis.master.conf
+cp /etc/redis.conf /etc/redis.slave.conf
+rm -f /etc/redis.conf
+
+sed -i 's/bind 127\.0\.0\.1/#bind 127\.0\.0\.1/g' /etc/redis.master.conf
+sed -i 's/bind 127\.0\.0\.1/#bind 127\.0\.0\.1/g' /etc/redis.slave.conf
+sed -i 's/# slaveof <masterip> <masterport>/slaveof redis-master 6379/g' /etc/redis.slave.conf
+
